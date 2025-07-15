e24786feb2ff6143703073757133a5da331abf06
Sherwin Romano <sherwinjayromano@gmail.com>
2025-07-16 06:37:55 +0800

    Changed preconnect to preload font

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.bunny.net" />
+        <link rel="preload" href="https://fonts.bunny.net" />
         <link
           href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700"
           rel="stylesheet"
